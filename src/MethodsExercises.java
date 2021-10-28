7f894502dfd4108514a74ad680ebabf93f401621
Robert Allison <robert.r.allison4@gmail.com>
2021-10-28 15:49:48 -0500

    setting up util

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,39 +1,39 @@
-import java.util.Scanner;
-public class MethodsExercises {
-
-    public static void main(String[] args) {
-//    addition(5,6);
-    }
-
-//    public static int getInteger(int min, int max){
-//        System.out.print("Enter a number between 1 and 10: ");
-//        int userInput = getInteger(1, 10);
+//import java.util.Scanner;
+//public class MethodsExercises {
+//
+//    public static void main(String[] args) {
+////    addition(5,6);
 //    }
-
-
-    public static double addition(int num1, int num2) {
-        return num1 + num2;
-    }
-
-    public static double subtraction(int num1, int num2) {
-        return num1 - num2;
-    }
-
-    public static double multiplication(int num1, int num2) {
-        return num1 * num2;
-    }
-
-    public static double divison(int num1, int num2) {
-        return num1 / num2;
-    }
-
-    public static double modulus(int num1, int num2) {
-        return num1 % num2;
-    }
-
-    public static int addition(int num1, int num2, int num3){
-        
-    }
-
-}
+//
+////    public static int getInteger(int min, int max){
+////        System.out.print("Enter a number between 1 and 10: ");
+////        int userInput = getInteger(1, 10);
+////    }
+//
+//
+//    public static double addition(int num1, int num2) {
+//        return num1 + num2;
+//    }
+//
+//    public static double subtraction(int num1, int num2) {
+//        return num1 - num2;
+//    }
+//
+//    public static double multiplication(int num1, int num2) {
+//        return num1 * num2;
+//    }
+//
+//    public static double divison(int num1, int num2) {
+//        return num1 / num2;
+//    }
+//
+//    public static double modulus(int num1, int num2) {
+//        return num1 % num2;
+//    }
+//
+//    public static int addition(int num1, int num2, int num3){
+//
+////    }
+//
+//}
 
