de20e542e8e50f4384379c62ed3bbcd4d3b647b6
Robert Allison <robert.r.allison4@gmail.com>
2021-11-01 13:39:20 -0500

    need to fix people array

---
+++ src/ArraysExercises.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class ArraysExercises {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+    Person[] people = {new Person("Mia"), new Person("Emma"), new Person("Vivienne")};
+    for (people x : Person) {
+        System.out.println(Person);
+    }
+}
