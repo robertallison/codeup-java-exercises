a1562ea9833d6984daa0b3f65ec0a8b9cf3ff478
Robert Allison <robert.r.allison4@gmail.com>
2021-11-01 09:46:36 -0500

    exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,5 +1,21 @@
 package util;
 
+import java.util.Scanner;
+
 public class Input {
-    
+    private Scanner scanner = new Scanner(System.in);
+
+    public String getString() {
+        System.out.println("Return string: ");
+        String anyString = scanner.nextLine();
+        return anyString;
+    }
+
+    public static void main(String[] args) {
+        Input input = new Input();
+        input.getString();
+        System.out.println(input.getString());
+    }
 }
+
+
