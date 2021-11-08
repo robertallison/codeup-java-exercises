6cf78deaae4d23d3099dc9a7ee81b51dcc52556a
Robert Allison <robert.r.allison4@gmail.com>
2021-11-08 09:14:33 -0600

    student app

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,4 +1,17 @@
 package grades;
 
+import java.util.ArrayList;
+
 public class Student {
+    private String studentName;
+    private ArrayList<Integer> grades;
+
+    public Student(String studentName) {
+        this.studentName = studentName;
+        this.grades = new ArrayList<Integer>();
+    }
+
+    public String returnName() {
+        return this.studentName;
+    }
 }
